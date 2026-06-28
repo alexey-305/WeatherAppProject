6ee9616f263ddd55b91af69d6a28e81298d308f9
alexey-305 <enot.in100g1@gmail.com>
2026-06-28 07:11:48 +0400

    Initial Commit

---
+++ WeatherAppProject/WeatherAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppProjectApp.swift
+//  WeatherAppProject
+//
+//  Created by Алексей Кузнецов on 28.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
